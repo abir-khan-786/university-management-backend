0a906ad6f955d2d30dc8f90a640a7b367fe9811b
abir-khan-786 <abirk2day@gmail.com>
2024-04-26 11:16:48 +0600

    test

--- src/app.ts
+++ src/app.ts
@@ -6,17 +6,8 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-app.get('/res', (req: Request, res: any) => {
+app.get('/', (req: Request, res: Response) => {
   res.send('Working sucessfly ')
-})
-app.get('/sdfsa', (req: Request, res: any) => {
-  res.send('Backend Working   ')
-})
-app.get('/sfd', (req: Request, res: any) => {
-  res.send('Backend Working   ')
-})
-app.get('/', (req: Request, res: any) => {
-  res.send('Backend Working   ')
 })
 
 export default app
