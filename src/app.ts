bd0f003066c6b56e21037cb3800682dcbf1d51f8
abir-khan-786 <abirk2day@gmail.com>
2024-04-29 21:14:30 +0600

    setup lint-stage

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,7 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-app.get('/', (req: Request, res: res) => {
+app.get('/', (req: Request, res: Response) => {
   res.send('database is connented successfully')
 })
 
